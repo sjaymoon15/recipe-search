df7e47397c4c1b9cfde9ebd32fd8356a01d22c1a
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-03 13:28:01 -0500

    change API library

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,15 @@
 import axios from 'axios';
 
-const APP_ID = '8da6e828';
-const APP_KEY = '3f9a2cf7368a3f795c488ab5659e41ca';
-const ROOT_URL = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}`;
-// const API_KEY = '6e4c80df2c0b82d8bc138d3daa28f2f5';
-// const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const APP_KEY = '0b9efd9649b8d968eba12e3db27f5e5a';
+const ROOT_URL = `http://food2fork.com/api/search?&key=${APP_KEY}`;
+
 export const SEARCH_RECIPES = 'SEARCH_RECIPES';
 
 export function searchRecipes(searchMe){
 	const url = `${ROOT_URL}&q=${searchMe}`;
-
-	// const url = `${ROOT_URL}&q=${searchMe},us`;
 	const request = axios.get(url);
 	return {
 		type: SEARCH_RECIPES,
 		payload: request
 	};
-	// return (dispatch) => {
-	// 	request.then(({data}) => {
-	// 		dispatch({ 
-	// 			type: SEARCH_RECIPES,
-	// 			payload: data
-	// 		});
-	// 	})
-	// };
 }
